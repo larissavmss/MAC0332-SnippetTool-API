8bf66abe219783432a3727eb90393ea9f432570a
Gabriel Arrais <arrais.gbr@usp.br>
2023-08-28 12:30:59 -0300

    project kickoff

---
+++ src/main/java/br/com/usp/mac0332/snippettool/SnippettoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.usp.mac0332.snippettool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippettoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippettoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/usp/mac0332/snippettool/SnippettoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usp.mac0332.snippettool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippettoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
