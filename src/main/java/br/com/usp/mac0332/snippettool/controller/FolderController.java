8fd500ce745f53955b5320973adc19834c71eb4f
Gabriel Arrais <arrais.gbr@usp.br>
2023-10-24 17:35:00 -0300

    fix endpoints and responses for folder entity

--- src/main/java/br/com/usp/mac0332/snippettool/controller/FolderController.java
+++ src/main/java/br/com/usp/mac0332/snippettool/controller/FolderController.java
@@ -1,29 +1,55 @@
 package br.com.usp.mac0332.snippettool.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.usp.mac0332.snippettool.model.Folder;
 import br.com.usp.mac0332.snippettool.service.FolderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("folder")
 public class FolderController {
-    @Autowired
-    FolderService folderService;
-    @PostMapping("/add")
-    public void addFolder(@RequestBody Folder folder){
-        folderService.addFolder(folder);
-    }
 
-    @GetMapping("/findByName")
-    public Folder findByName(@RequestParam("name") String name){
-        return folderService.findByName(name);
-    }
+	@Autowired
+	FolderService folderService;
 
-    @GetMapping("/getAll")
-    public List<Folder> getAll(){
-        return folderService.getAll();
-    }
+	@PostMapping
+	public ResponseEntity<Folder> addFolder(@RequestBody Folder folder) {
+		Folder response = folderService.addFolder(folder);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Folder>> getAll() {
+		List<Folder> response = folderService.getAll();
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/{folderId}")
+	public ResponseEntity<Folder> findByName(@PathVariable("name") String name) {
+		Folder response = folderService.findByName(name);
+		return ResponseEntity.ok(response);
+	}
+
+	@PutMapping("/{folderId}")
+	public ResponseEntity<Folder> updateFolder(@PathVariable Integer folderId, @RequestBody Folder updatedFolder) {
+		Folder response = folderService.updateFolder(folderId, updatedFolder);
+		return ResponseEntity.ok(response);
+	}
+
+	@DeleteMapping("/{folderId}")
+	public ResponseEntity<Void> deleteFolder(@PathVariable Integer folderId) {
+		folderService.deleteFolder(folderId);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/br/com/usp/mac0332/snippettool/service/FolderService.java
+++ src/main/java/br/com/usp/mac0332/snippettool/service/FolderService.java
@@ -1,27 +1,43 @@
 package br.com.usp.mac0332.snippettool.service;
 
-import br.com.usp.mac0332.snippettool.model.Folder;
-import br.com.usp.mac0332.snippettool.repository.FolderRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import br.com.usp.mac0332.snippettool.model.Folder;
+import br.com.usp.mac0332.snippettool.repository.FolderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class FolderService {
 
-    @Autowired
-    FolderRepository repo;
+	@Autowired
+	FolderRepository repository;
 
-    public void addFolder(Folder folder) {
-        repo.save(folder);
-    }
+	@Transactional
+	public Folder addFolder(Folder folder) {
+		return repository.save(folder);
+	}
 
-    public Folder findByName(String name) {
-        return repo.findByName(name);
-    }
+	public Folder findByName(String name) {
+		return repository.findByName(name);
+	}
 
-    public List<Folder> getAll() {
-        return repo.findAll();
-    }
+	public List<Folder> getAll() {
+		return repository.findAll();
+	}
+
+	@Transactional
+	public Folder updateFolder(Integer folderId, Folder updatedFolder) {
+		Folder existingFolder = repository.findById(folderId)
+				.orElseThrow(() -> new EntityNotFoundException("Folder not found with id: " + folderId));
+		existingFolder.update(updatedFolder);
+		return repository.save(existingFolder);
+	}
+
+	public void deleteFolder(Integer snippetId) {
+		repository.deleteById(snippetId);
+	}
 }
