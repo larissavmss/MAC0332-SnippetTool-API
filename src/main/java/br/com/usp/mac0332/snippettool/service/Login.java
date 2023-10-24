044ad2dc4682f9e7e18be7613f5dcfcaf46b58f2
Naythan <naythann88@gmail.com>
2023-10-24 09:00:58 -0300

    Add in progress auth

---
+++ src/main/java/br/com/usp/mac0332/snippettool/service/Login.java
@@ -0,0 +1,11 @@
+package br.com.usp.mac0332.snippettool.service;
+
+
+import lombok.Getter;
+
+@Getter
+public class Login {
+    private String username;
+    private String password;
+    private String email;
+}
