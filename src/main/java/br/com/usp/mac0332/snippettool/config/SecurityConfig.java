f891f37378082ac3c662e5dbc9a04d2c4a3f0967
Naythan <naythann88@gmail.com>
2023-10-29 16:21:58 -0300

    Add authentication by cookie

--- src/main/java/br/com/usp/mac0332/snippettool/config/SecurityConfig.java
+++ src/main/java/br/com/usp/mac0332/snippettool/config/SecurityConfig.java
@@ -1,19 +1,19 @@
 package br.com.usp.mac0332.snippettool.config;
 
 import br.com.usp.mac0332.snippettool.service.UserService;
-import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 @Configuration
@@ -26,50 +26,26 @@ public class SecurityConfig {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private CustomAuthenticationProvider authProvider;
 
     @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(authProvider);
-        return authenticationManagerBuilder.build();
-    }
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth)
-//            throws Exception {
-//        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
-//    }
+        return new ProviderManager(authenticationProvider);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         // We are disabling CSRF so that our forms don't complain about a CSRF token.
         // Beware that it can create a security vulnerability
         return http.csrf(AbstractHttpConfigurer::disable)
-                // Here we are configuring our login form
-//                .formLogin(formLogin -> {
-//                            formLogin
-//                                    .loginPage("/login") // Login page will be accessed through this endpoint. We will create a controller method for this.
-//                                    .loginProcessingUrl("/login-processing") // This endpoint will be mapped internally. This URL will be our Login form post action.
-//                                    .usernameParameter("username")
-//                                    .passwordParameter("password")
-//                                    .permitAll() // We re permitting all for login page
-//                                    .defaultSuccessUrl("/") // If the login is successful, user will be redirected to this URL.
-//                                    .failureUrl("/login?error=true"); // If the user fails to login, application will redirect the user to this endpoint
-//                        }
-//                )
-                .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers ("/login-test").permitAll()
-                            .anyRequest().rememberMe();
-                })
-                .rememberMe(httpSecurityRememberMeConfigurer ->
-                    httpSecurityRememberMeConfigurer
-                            .alwaysRemember(true)
-                            .tokenValiditySeconds(60)
-                            .rememberMeCookieName("teste")
-                            .key("segredo")
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login", "/register").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .logout(logout ->
@@ -77,7 +53,6 @@ public class SecurityConfig {
                                 .logoutUrl("/logout")
                                 .logoutSuccessUrl("/")
                 )
-                .addFilterBefore((Filter) new CheckAuthCookieFilter(), BasicAuthenticationFilter.class)
                 .build();
     }
 
