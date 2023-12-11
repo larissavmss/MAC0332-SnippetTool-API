d4ce3ca73ca2a32d82829e546b1d044b97693890
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-10 22:00:42 -0300

    fix some imports

--- src/main/java/br/com/usp/mac0332/snippettool/repository/FolderRepository.java
+++ src/main/java/br/com/usp/mac0332/snippettool/repository/FolderRepository.java
@@ -1,15 +1,15 @@
 package br.com.usp.mac0332.snippettool.repository;
 
-import br.com.usp.mac0332.snippettool.model.Folder;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import br.com.usp.mac0332.snippettool.model.Folder;
 
 public interface FolderRepository extends JpaRepository<Folder, Integer> {
 
     Folder findByName(String name);
 
-    Folder save(Folder folder);
-
     List<Folder> findAllByUserId(Integer id);
+    
 }
--- src/main/java/br/com/usp/mac0332/snippettool/repository/UserRepository.java
+++ src/main/java/br/com/usp/mac0332/snippettool/repository/UserRepository.java
@@ -1,16 +1,14 @@
 package br.com.usp.mac0332.snippettool.repository;
 
-import br.com.usp.mac0332.snippettool.model.Folder;
-import br.com.usp.mac0332.snippettool.model.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import br.com.usp.mac0332.snippettool.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
-
-    User save(User user);
 
     List<User> findAll();
 
