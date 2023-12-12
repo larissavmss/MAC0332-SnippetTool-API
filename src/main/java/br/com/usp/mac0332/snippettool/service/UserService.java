1dc165f527d92afc20cacef847ad5dc1350f73c7
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-12 16:57:09 -0300

    creating default folder for every user

--- src/main/java/br/com/usp/mac0332/snippettool/service/UserService.java
+++ src/main/java/br/com/usp/mac0332/snippettool/service/UserService.java
@@ -10,22 +10,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.usp.mac0332.snippettool.dto.auth.AuthRegisterDto;
+import br.com.usp.mac0332.snippettool.dto.folder.FolderCreateDto;
 import br.com.usp.mac0332.snippettool.dto.user.UserResponseDto;
 import br.com.usp.mac0332.snippettool.dto.user.UserUpdateDto;
 import br.com.usp.mac0332.snippettool.model.User;
 import br.com.usp.mac0332.snippettool.repository.UserRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
 
 	@Autowired
 	UserRepository repo;
+	
+	@Autowired
+	FolderService folderService;
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
+	@Transactional
 	public UserResponseDto create(AuthRegisterDto registerDto) {
 		User user = new User(registerDto, passwordEncoder.encode(registerDto.password()));
+		FolderCreateDto folderCreateDto = new FolderCreateDto("Default");
+		folderService.addFolder(folderCreateDto, user);
 		UserResponseDto userResponseDto = new UserResponseDto(repo.save(user));
 		return userResponseDto;
 	}
