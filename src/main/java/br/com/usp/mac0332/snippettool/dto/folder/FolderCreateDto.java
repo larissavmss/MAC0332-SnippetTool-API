181c99b34cdd7ad95c5234e8c04c7defe352ff6b
larissavmss <larissavitoriamedeiros@usp.br>
2023-12-13 18:51:07 -0300

    Add JsonIgnoreProperties and JsonFormat to dtos

--- src/main/java/br/com/usp/mac0332/snippettool/dto/folder/FolderCreateDto.java
+++ src/main/java/br/com/usp/mac0332/snippettool/dto/folder/FolderCreateDto.java
@@ -2,6 +2,11 @@ package br.com.usp.mac0332.snippettool.dto.folder;
 
 import jakarta.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FolderCreateDto(@NotBlank String name) {
 
 }
