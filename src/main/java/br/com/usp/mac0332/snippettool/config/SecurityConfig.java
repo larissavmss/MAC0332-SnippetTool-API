ad8225eca138a6f2e848691d2e7126a92005e944
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-12 21:05:53 -0300

    fix security configs

--- src/main/java/br/com/usp/mac0332/snippettool/config/SecurityConfig.java
+++ src/main/java/br/com/usp/mac0332/snippettool/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.usp.mac0332.snippettool.config;
 
-import br.com.usp.mac0332.snippettool.service.UserService;
+import static org.springframework.http.HttpMethod.POST;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,46 +16,40 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import br.com.usp.mac0332.snippettool.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
+	@Bean
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
+		return new ProviderManager(authenticationProvider);
+	}
 
-        return new ProviderManager(authenticationProvider);
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        // We are disabling CSRF so that our forms don't complain about a CSRF token.
-        // Beware that it can create a security vulnerability
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/login", "/register").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .logout(logout ->
-                        logout
-                                .logoutUrl("/logout")
-                                .logoutSuccessUrl("/")
-                )
-                .build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// We are disabling CSRF so that our forms don't complain about a CSRF token.
+		// Beware that it can create a security vulnerability
+		return http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests((authorize) -> authorize
+						.requestMatchers(POST, "auth/login").permitAll()
+						.requestMatchers(POST, "auth/register").permitAll()
+						.anyRequest().authenticated())
+				.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+						.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/")).build();
+	}
 
 }
-
