99242488b4745ed15270be21e44447edf3178c1b
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-13 16:56:45 -0300

    removing unused query

--- src/main/java/br/com/usp/mac0332/snippettool/repository/UserRepository.java
+++ src/main/java/br/com/usp/mac0332/snippettool/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package br.com.usp.mac0332.snippettool.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.usp.mac0332.snippettool.model.User;
@@ -9,7 +7,5 @@ import br.com.usp.mac0332.snippettool.model.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
-
-    List<User> findAll();
 
 }
