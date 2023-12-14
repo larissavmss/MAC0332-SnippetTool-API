4e206bc21531030016eafd005c96fa6075aa5ef9
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-14 10:22:30 -0300

    removing unused endpoints and methods

--- src/main/java/br/com/usp/mac0332/snippettool/service/UserService.java
+++ src/main/java/br/com/usp/mac0332/snippettool/service/UserService.java
@@ -1,7 +1,5 @@
 package br.com.usp.mac0332.snippettool.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,7 +10,6 @@ import org.springframework.stereotype.Service;
 import br.com.usp.mac0332.snippettool.dto.auth.AuthRegisterDto;
 import br.com.usp.mac0332.snippettool.dto.folder.FolderCreateDto;
 import br.com.usp.mac0332.snippettool.dto.user.UserResponseDto;
-import br.com.usp.mac0332.snippettool.dto.user.UserUpdateDto;
 import br.com.usp.mac0332.snippettool.model.User;
 import br.com.usp.mac0332.snippettool.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -38,25 +35,10 @@ public class UserService implements UserDetailsService {
 		return userResponseDto;
 	}
 
-	public List<UserResponseDto> findAll() {
-		List<User> users = repo.findAll();
-		List<UserResponseDto> usersResponseDto = users.stream().map(UserResponseDto::new).toList();
-		return usersResponseDto;
-	}
-
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = repo.findByUsername(username);
 		return new MyUserDetails(user);
 	}
 
-	public void delete(User user) {
-		repo.delete(user);
-	}
-
-	public UserResponseDto update(UserUpdateDto userUpdateDto, User user) {
-		user.update(userUpdateDto, passwordEncoder.encode(userUpdateDto.password()));
-		UserResponseDto userResponseDto = new UserResponseDto(repo.save(user));
-		return userResponseDto;
-	}
 }
