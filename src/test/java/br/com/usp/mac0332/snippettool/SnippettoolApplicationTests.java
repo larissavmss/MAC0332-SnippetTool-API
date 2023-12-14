cce1e8aa320013f6adaad2ed7bb3ed6199bdeb9f
Gabriel Arrais <arrais.gbr@usp.br>
2023-12-14 15:26:34 -0300

    removing default test of spring

--- src/test/java/br/com/usp/mac0332/snippettool/SnippettoolApplicationTests.java
+++ src/test/java/br/com/usp/mac0332/snippettool/SnippettoolApplicationTests.java
@@ -1,12 +1,10 @@
 package br.com.usp.mac0332.snippettool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SnippettoolApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
